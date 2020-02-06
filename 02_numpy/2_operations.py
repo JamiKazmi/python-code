b24e3dae4e77082137a3517864f72b285afc40fd
JamiKazmi <jamishah110@gmail.com>
2020-02-06 23:12:42 +0500

    Initial Commits

--- 02_numpy/2_operations.py
+++ 02_numpy/2_operations.py
@@ -3,9 +3,22 @@ import time
 import numpy as np
 
 my_array = np.array([1, 2, 3])
+second_array = np.array([
+    (1, 2, 3, 4, 5, 6),
+    (7, 8, 9, 10, 11, 12)
+])
 
 # returns the type of array
 print(my_array.ndim)
 
 # returns the byte size
 print(my_array.itemsize)
+
+# returns datatype
+print(my_array.dtype)
+
+# return size of array
+print(second_array.size)
+
+# return the shape of array
+print(second_array.shape)
